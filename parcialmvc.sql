7938537cf1d07eeb0446d4b443f2d619514c14e2
marving2015 <link_marvin@outlook.com>
2015-09-24 08:59:48 -0600

    parcialmvc.sql
    Actualizacion de BD

--- parcialmvc.sql
+++ parcialmvc.sql
@@ -7,8 +7,7 @@
 -- Versión del servidor: 5.6.24
 -- Versión de PHP: 5.6.8
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -26,31 +25,20 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `empresario`
 --
 
-CREATE TABLE IF NOT EXISTS `empresario` (
-  `idempresario` int(11) NOT NULL,
-  `nombres` varchar(100) NOT NULL,
-  `apellidos` varchar(100) NOT NULL,
-  `direccion` varchar(150) NOT NULL,
-  `ciudad` varchar(100) NOT NULL,
-  `sexo` varchar(1) NOT NULL,
-  `telefono` varchar(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
-
+CREATE TABLE empresario(
+	  idempresario int(11) AUTO_INCREMENT PRIMARY KEY,
+    nombres varchar(100) not null,
+    apellidos varchar(100) not null,
+    direccion varchar(100) not null,
+    ciudad varchar(100) not null,
+    sexo varchar(1) not null,
+    telefono varchar(11) not null
+);
 --
 -- Volcado de datos para la tabla `empresario`
 --
 
-INSERT INTO `empresario` (`idempresario`, `nombres`, `apellidos`, `direccion`, `ciudad`, `sexo`, `telefono`) VALUES
-(1, 'Bonnie', 'Porter', 'Na Kae', 'San Salvador', 'F', '610-83-2556'),
-(2, 'Samuel', 'Moore', 'Uberaba', 'Santa Ana', 'M', '670-75-5396'),
-(3, 'Lois', 'Mason', 'Hujia', 'San Vicente', 'F', '298-42-2736'),
-(4, 'Ryan', 'Patterson', 'Liushui', 'San Salvador', 'M', '112-28-8714'),
-(5, 'Jessica', 'Reid', 'Jiuxian', 'Santa Ana', 'F', '193-61-0516'),
-(6, 'Denise', 'Stone', 'Kafue', 'San Vicente', 'F', '574-65-6973'),
-(7, 'Harry', 'Lee', 'Alicia', 'San Salvador', 'M', '302-95-7907'),
-(8, 'Fred', 'Welch', 'Braknec', 'Santa Ana', 'M', '574-65-6974'),
-(9, 'Todd', 'Castillo', 'Zbraslav', 'San Vicente', 'M', '168-05-6765'),
-(10, 'Mary', 'Montgomery', 'Huiwan', 'San Salvador', 'F', '874-22-4709');
+
 
 --
 -- Índices para tablas volcadas
@@ -59,8 +47,7 @@ INSERT INTO `empresario` (`idempresario`, `nombres`, `apellidos`, `direccion`, `
 --
 -- Indices de la tabla `empresario`
 --
-ALTER TABLE `empresario`
-  ADD PRIMARY KEY (`idempresario`);
+
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -69,8 +56,7 @@ ALTER TABLE `empresario`
 --
 -- AUTO_INCREMENT de la tabla `empresario`
 --
-ALTER TABLE `empresario`
-  MODIFY `idempresario` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
